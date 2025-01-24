cb6feef23baf4c3563fcbdbb6053e1cc78741be6
frrikk <frikk.a.odlolien@tietoevry.com>
2025-01-24 15:38:04 +0100

    feat: adds react query and eslint ruling

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,15 @@ import "./index.css";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router";
 import App from "@/app/app";
+import { QueryClient } from "@tanstack/query-core";
+import { QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
 
 createRoot(document.getElementById("root")!).render(
   <BrowserRouter>
-    <App />
+    <QueryClientProvider client={queryClient}>
+      <App />
+    </QueryClientProvider>
   </BrowserRouter>,
 );
